bbc36c3ca945c4195a80685bdacbac33b3a92372
wifelette <LeahSilber@gmail.com>
2019-11-19 19:15:04 -0800

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,10 @@
-class Helpers
+module Helpers
+  module_function
+  def current_user(session)
+    User.find_by(id: session[:user_id])
+  end
+
+  def is_logged_in?(session)
+    !!session[:user_id]
+  end
 end
