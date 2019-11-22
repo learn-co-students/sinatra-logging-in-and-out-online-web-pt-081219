834d50868a7ebe29bcc5696c0fb7eae4855da589
Joseph Sesti <github email address>
2019-11-22 13:08:16 -0600

    Done.

--- db/migrate/01_create_users.rb
+++ db/migrate/01_create_users.rb
@@ -1,6 +1,6 @@
-class CreateUser < ActiveRecord::Base
+class CreateUsers < ActiveRecord::Base
   def change
-    create_table :user do |u|
+    create_table :users do |u|
       u.string :username
       u.string :password
       u.integer :balance
