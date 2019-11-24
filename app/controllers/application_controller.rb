0a5f5048f33a334860d7d7b5c6b6886d9c988a1e
Joseph Sesti <github email address>
2019-11-24 13:51:19 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    id = session[:user_id]
+    @current_user = User.find_by_id(session[:user_id])
     if @current_user
       erb :account
     else
